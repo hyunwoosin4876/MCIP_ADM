a70403a6efa6bf7ed64371f233235822c3330b89
ryu <ryuryujie@gmail.com>
2023-05-17 17:48:51 +0900

    pub

--- MCIP_ADM/doc/publish/1.0/js/commonUI.js
+++ MCIP_ADM/doc/publish/1.0/js/commonUI.js
@@ -22,5 +22,44 @@ $(function(){
 			element.siblings('li').find('ul').slideUp();
 		}
 	});
-
+	
+	//파일추가
+	addFileAttach();
+	$('.btn_fileAdd').off('click').on('click', function(){
+		addFileAttach('.file_group');
+	});
 });
+
+
+//파일추가
+function addFileAttach(targetGroup) {
+	var file = '<div class="file_attach">' 
+	+'   <label class="label"><input type="file" class="upload_hidden"></label>'  
+	+'   <input value="" placeholder="파일검색" class="upload_name">'
+	+'   <button type="button" class="btn_fileDel">파일삭제</button>'
+	+'</div>';
+	$(targetGroup).append(file);
+	setAttachEvent();
+}
+
+//파일추가 이벤트 설정
+function setAttachEvent() {
+	$('.upload_hidden').off('change').on('change', function(){
+		if(window.FileReader){
+		var filename = $(this)[0].files[0].name;
+		} else {
+		var filename = $(this).val().split('/').pop().split('\\').pop();
+		}
+		$(this).parent().siblings('.upload_name').val(filename);
+	});
+
+	$('.btn_fileDel').off('click').on('click', function(){
+		var attachCnt = $(this).parents('.file_group').find('.file_attach').length;
+		$(this).parents('.file_attach').remove();
+		if(attachCnt == 1) addFileAttach('.file_group');
+	});
+
+	$('.file_attach .upload_name').off('click').on('click', function(){
+		$(this).parents('.file_attach').find('.upload_hidden').click();
+	});
+}
--- MCIP_ADM/doc/publish/1.0/js/datepicker.js
+++ MCIP_ADM/doc/publish/1.0/js/datepicker.js
@@ -20,39 +20,4 @@ $(function(){
 	$('.btn_calendar').on('click', function(){
 			$(this).prev().focus();
 	});
-
-
-		
-	$(".timepicker").timepicker({
-
-		lang : 'ko',
-		//timeFormat: 'HH:mm',
-		timeFormat: 'p h:mm ',
-		interval: 1,
-		startTime: '00:00',
-		//dynamic: false,
-		dropdown: true,
-		scrollbar: true
-
-	
-
-	});
-	
-	/*
-	$(".datetimepicker").datetimepicker({
-		dateFormat:'yy-mm-dd',
-		monthNamesShort:[ '1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월' ],
-		dayNamesMin:[ '일', '월', '화', '수', '목', '금', '토' ],
-		changeMonth:true,
-		changeYear:true,
-		showMonthAfterYear:true,
-
-		// timepicker 설정
-		timeFormat:'HH:mm:ss',
-		controlType:'select',
-		oneLine:true,
-	});
-	*/
-
-
 });
