87c20532317681e331cd77e6a6f691cfee846cc5
이상민 <smlee1@ispark.kr>
2023-05-19 15:40:28 +0900

    코드관리, 레이아웃 커밋

---
+++ MCIP_ADM/src/main/webapp/WEB-INF/views/pub/js/datepicker.js
@@ -0,0 +1,23 @@
+$(function(){
+	
+	$.datepicker.regional['ko'] = {
+		prevText: '이전 달',
+        nextText: '다음 달',
+        monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+        monthNamesShort: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+        dayNames: ['일', '월', '화', '수', '목', '금', '토'],
+        dayNamesShort: ['일', '월', '화', '수', '목', '금', '토'],
+        dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
+        showMonthAfterYear: true,
+        yearSuffix: '년',
+		dateFormat:'yy-mm-dd'
+
+	};
+	$.datepicker.setDefaults($.datepicker.regional['ko']);
+
+	$('.datepicker').datepicker();
+
+	$('.btn_calendar').on('click', function(){
+			$(this).prev().focus();
+	});
+});
